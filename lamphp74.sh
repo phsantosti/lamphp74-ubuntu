5c6041d18b74cc74248c8d3609704b81108e5ae2
Pedro Henrique dos Santos <phsantosti@gmail.com>
2024-04-26 16:37:20 -0300

    Update lamphp74.sh

--- lamphp74.sh
+++ lamphp74.sh
@@ -50,6 +50,8 @@ sudo apt install -y php7.4
 # Instalação das extensões PHP
 sudo apt-get install -y php7.4-cli php7.4-json php7.4-common php7.4-mysql php7.4-zip php7.4-gd php7.4-mbstring php7.4-curl php7.4-xml php7.4-bcmath
 
+sudo apt install composer -y
+
 # Remoção de instalações prévias do Certbot
 sudo apt-get remove certbot
 
